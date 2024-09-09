e530c6a746863bd3dd2305e2286f66568cb4b92b
Sahil Gupta <138691394+11-sg@users.noreply.github.com>
2024-09-09 22:11:10 +0530

    Add files via upload

---
+++ portal.js
@@ -0,0 +1,11 @@
+const img=document.querySelector(".l");
+const img1=document.querySelector(".t");
+
+
+img.addEventListener("click" , ()=> {
+    alert("LandLord");
+
+})
+img1.addEventListener("click" , ()=> {
+    alert("Tenant");
+})
